ad67c6c25e2c50b607320857ac898af284f881fd
Brian Painter <Brian@Brians-Mac-mini.home>
2017-11-23 09:45:27 -0500

    Initial configuration

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('clean:build', function () {
+  return del([
+    'dist/**/*'
+  ]);
+});
+
+gulp.task('cleanBuild', ['clean:build']);
